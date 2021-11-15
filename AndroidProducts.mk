d4a6eb2ef03e8a704e0132b8bfabe3d6e7faab65
El Dainosor <eldainosor@gmail.com>
2021-11-14 23:25:59 -0300

    bootleg bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -23,9 +23,9 @@
 
 # Inherit Redmi Note 7 device config
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/evolution_lavender.mk
+    $(LOCAL_DIR)/bootleg_lavender.mk
 
 COMMON_LUNCH_CHOICES := \
-    evolution_lavender-eng \
-    evolution_lavender-userdebug \
-    evolution_lavender-user
+    bootleg_lavender-eng \
+    bootleg_lavender-userdebug \
+    bootleg_lavender-user
